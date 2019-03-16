2775d80dfcab51fe94f989ac17926d467f9a04b2
o.vdovichenko <o.vdovichenko@student.csn.khai.edu>
2019-03-16 21:31:17 +0200

    Update Controller.js

--- app/js/Controller.js
+++ app/js/Controller.js
@@ -1,15 +1,17 @@
+
 $( document ).ready(function() {
   $('#comment').on('click',function(){
     var named = prompt("Введите имя коментатора", "Лилия Семеновна");
+    if(named == null) named = 'Пользователь № '+ new Date().getMilliseconds();
     var texted = $('textarea').val();
     $('textarea').val('');
     var monthed = new Date().toLocaleString('ru', {month: 'long'});
     if(~monthed.indexOf("й") || ~monthed.indexOf("ь")){
       if(~monthed.indexOf("й")){
-       monthed = monthed.substring(monthed.indexOf("й"))
+       monthed = monthed.substring(monthed.indexOf("й"));
       }
       if(~monthed.indexOf("ь")){
-       monthed = monthed.substring(monthed.indexOf("ь"))
+       monthed = monthed.substring(monthed.indexOf("ь"));
       }
       monthed += 'я';
     }
@@ -22,6 +24,20 @@ $( document ).ready(function() {
     "</span> <span class = 'udate'>" + timed +
     "</span> <div class = 'said'>" + texted +
     "</div></div>"
-  );
+      );
   });
 });
+
+var controled = false;
+$(document).keyup(function (e) {
+  if(e.which == 17) controled = false;
+}).keydown(function (e) {
+  if(e.which == 17) controled=true;
+  if(e.which == 13 && controled == true) {
+    $('#comment').click();
+    controled=false;
+  }
+});
+
+//17
+//13
